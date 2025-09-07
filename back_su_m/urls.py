500221b5944a2e7f1f447eb67744be82e84362f7
adilhan <adilhansatymkulov40@gmaill.com>
2025-09-07 15:02:43 +0600

    commit

--- back_su_m/urls.py
+++ back_su_m/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('news.urls')),  # API endpoints
     path('api/', include('publications.urls')),  # API endpoints
     path('api/', include('news.urls')),  # News API endpoints
     path('research/', include('research.urls')),  # Research API endpoints
